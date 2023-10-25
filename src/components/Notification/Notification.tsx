abda3f5e9184543f7c7106004924a1ec6211edd2
KuchkarovMurodbek <kuchkarovmurodbek@gmail.com>
2023-10-25 21:15:12 +0500

    components is added

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,8 +1,28 @@
+import { toast } from "react-toastify";
 
-function Notification() {
-  return (
-    <div>Notification</div>
-  )
+interface IPorps {
+  message: string;
+  type: "success" | "error";
 }
+const Notification = ({ message, type }: IPorps) => {
+  const displayNotification = () => {
+    toast[type](message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
 
-export default Notification
+  return (
+    <div>
+      <button onClick={displayNotification}>Show Notification</button>
+    </div>
+  );
+};
+
+export default Notification;
