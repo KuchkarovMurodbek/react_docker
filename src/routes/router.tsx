53429061d78a31f8ad18abf0285e02af8c5c0f8f
KuchkarovMurodbek <kuchkarovmurodbek@gmail.com>
2023-10-26 22:11:30 +0500

    Children Routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/Error/ErrorPage";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
+import Blog from "../pages/Blog/Blog";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:  <Home/>,
+    Component: Home,
+    // element:  <Home/>,
     errorElement: <ErrorPage />,
     // loader: () => ({ message: "Hello Data Router!" }),
     // Component() {
@@ -17,7 +19,18 @@ const router = createBrowserRouter([
   {
     path:'/about',
     element:<About/>
-  }
+  },
+  {
+    // Lifted blog splat route
+    path: "/blog/*",
+    children: [
+      // New blog index route
+      { index: true, Component: () => <h1>Blog Index</h1> },
+      // Blog subapp splat route added for /blog/posts matching
+      { path: "*", Component: Blog },
+    ],
+  },
+
 ]);
 
 export default router;
